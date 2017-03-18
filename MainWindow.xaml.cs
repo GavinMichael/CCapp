06cc647e933bf08463df462af253af17f18cc6bd
GavinMichael <gavin.michael.ftw@gmail.com>
2017-03-19 02:30:55 +0400

    Implemented Themes Completed the Help Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-using System.Net;
-using System.Diagnostics;
 
 namespace CCApp
 {
@@ -26,67 +17,64 @@ namespace CCApp
     {
         /* Shit that isnt done yet
          * 
-         * Updates not implemented
+         * Updates - Not Complete
+         * Theme - Complete
         */
 
 
         //this counts the entries for Label:counter
         int numCounter = 0;
+
+
+        //Entry point
         public MainWindow()
         {
             InitializeComponent();
+            //ThemeHandler("retro");
         }
-        
+
+
         //this takes the filepath as input pram and outputs the text into the main textbox
         public void TextHanlder(string _filePath)
         {
             //reads all the lines into a list
             List<string> content = File.ReadAllLines(_filePath).ToList();
             //loops through the list reading each line
+
             foreach (string line in content)
             {
                 //adds the line read from the txt file
                 mainTxtBox.AppendText(line);
+
                 //adds  a new line to the text box
                 mainTxtBox.AppendText("\n");
             }
         }
 
-        /* this space is for updates when it will be implemented
-        private void updatesBtn_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                WebClient web = new WebClient();
-                System.IO.Stream stream = web.OpenRead("http://pastebin.com/raw/RXpKzYB4");
-                using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
-                {
-                    string text = reader.ReadToEnd();
-                    updateTxtBox.AppendText(text);
-                }
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.Message);
-            }
-        }
-        */
-
-        //Top Menu Breaks Button click event - Implemented
+        //Top Buttons
         #region
+
+        //Break Button
         private void breakBtn_Click(object sender, RoutedEventArgs e)
         {
             Window1 breakWindow = new Window1();
             breakWindow.Show();
         }
 
-        //Top Menu Quit Button click event - Implemented
+        //Help Button
+        private void helpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            helpWindow hlpWindow = new helpWindow();
+            hlpWindow.Show();
+        }
+
+        //Quit Button
         private void mainQuitBtn_Click(object sender, RoutedEventArgs e)
         {
             App.Current.Shutdown();
         }
 
-        //Top Menu New Call Button click event - Implemented
+        //New Call
         private void newCallBtn_Click(object sender, RoutedEventArgs e)
         {
             //This method does 3 things
@@ -124,11 +112,13 @@ namespace CCApp
             #region
 
             //holds the line break
-            string lineBreak = "---------------------------------------------------------------- ";
+            string lineBreak = "----------------------------------------------------------- ";
             //Concatenate the line with current time and new line
             lineBreak = lineBreak + DateTime.Now.ToShortTimeString() + "\n";
             //Adds the string to the Main Text Box
-            mainTxtBox.AppendText(lineBreak);
+            int selectionIndex = mainTxtBox.SelectionStart;
+            mainTxtBox.Text = mainTxtBox.Text.Insert(selectionIndex, lineBreak);
+            mainTxtBox.SelectionStart = selectionIndex + lineBreak.Length;
 
             #endregion
         }
@@ -148,7 +138,7 @@ namespace CCApp
         //CRM
         private void shortcutsCrmBtn_Click(object sender, RoutedEventArgs e)
         {
-                Process.Start("https://agthiagroup.crm4.dynamics.com/");
+            Process.Start("https://agthiagroup.crm4.dynamics.com/");
         }
 
         //CBD KB
@@ -179,19 +169,19 @@ namespace CCApp
         //Power BI
         private void shortcutsPowerBiBtn_Click(object sender, RoutedEventArgs e)
         {
-                Process.Start("https://app.powerbi.com/groups/64d2e7e0-34ae-44da-a09f-4b4d61c7acb6/reports/dcf81a5b-8061-4b92-b5b2-faebdf856045/ReportSection");
+            Process.Start("https://app.powerbi.com/groups/64d2e7e0-34ae-44da-a09f-4b4d61c7acb6/reports/dcf81a5b-8061-4b92-b5b2-faebdf856045/ReportSection");
         }
 
         //Remotemail
         private void shortcutsRmtMailBtn_Click(object sender, RoutedEventArgs e)
         {
-                Process.Start("https://remotemail.agthia.com/owa");
+            Process.Start("https://remotemail.agthia.com/owa");
         }
 
         //Planned Leave
         private void shortcutsPlannedLeavesBtn_Click(object sender, RoutedEventArgs e)
         {
-                Process.Start("https://docs.google.com/spreadsheets/d/1LMt-GJw2ISLF-glQlbL-fZweqG6axwX5MkrWm0eUhuM/edit#gid=557340792");
+            Process.Start("https://docs.google.com/spreadsheets/d/1LMt-GJw2ISLF-glQlbL-fZweqG6axwX5MkrWm0eUhuM/edit#gid=557340792");
         }
 
         //Price Inquiry
@@ -199,14 +189,9 @@ namespace CCApp
         {
             Process.Start("https://goo.gl/forms/5laEXcIuAUk0IMo72");
         }
-
         #endregion
-        private void smNotAnsweringBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start(@"_sourceFiles\emails\smNotAnswering.msg");
-        }
 
-        //Here are the template button click events - !!!!!!!!!!!!!!!!! Not Complete
+        //Here are the template button click events
         #region
 
         //Delivery Issue Btn
@@ -294,5 +279,159 @@ namespace CCApp
         }
         #endregion
 
+        //Theme functionality here
+
+        private void ThemeHandler(object sender, RoutedEventArgs e)
+        {
+            //this captures the tag of the button clicked to evaluate later
+            var entry = ((Button)sender).Tag;
+
+            #region Theme Definitions
+            //white & black 
+            var white = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+            var black = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+
+            #region Theme One
+            var themeOneText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFAFA"));
+            var themeOneMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0098eb"));
+            var themeOneSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0F59EB"));
+            var themeOneNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E62900"));
+            var themeOneBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0FAEEB"));
+            #endregion End of Theme One
+
+            #region Theme Two
+            var themeTwoText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFAFA"));
+            var themeTwoMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EE47D5"));
+            var themeTwoSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EB00C8"));
+            var themeTwoNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6B005B"));
+            var themeTwoBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EE81D5"));
+            #endregion End of Theme Two
+
+            #region Theme Three
+            var themeThreeText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFAFA"));
+            var themeThreeMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EB5307"));
+            var themeThreeSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#B84105"));
+            var themeThreeNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#B83F00"));
+            var themeThreeBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EE9D74"));
+            #endregion End of Theme Three
+
+            #region Theme Four
+            var themeFourText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFAFA"));
+            var themeFourMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#186B00"));
+            var themeFourSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2AB800"));
+            var themeFourNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2AB800"));
+            var themeFourBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#35EB00"));
+            #endregion End of Theme Four
+
+            #region Theme Default
+            var themeDefaultText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+            var themeDefaultMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
+            var themeDefaultSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7f7f7f"));
+            var themeDefaultNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7f7f7f"));
+            var themeDefaultBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#b2b2b2"));
+            #endregion End of Theme Four
+
+            #region Theme Gavin
+            var themeGavinText = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
+            var themeGavinMainColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+            var themeGavinSubColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#323232"));
+            var themeGavinNewCallColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#323232"));
+            var themeGavinBorder = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#262626"));
+            #endregion End of Theme Gavin
+
+            #endregion End of Theme Definitions
+
+            switch (entry.ToString())
+            {
+                case "One":
+                    ThemeChanger(themeOneMainColor, themeOneSubColor, themeOneText, themeOneBorder, themeOneNewCallColor, white, black);
+                    break;
+                case "Two":
+                    ThemeChanger(themeTwoMainColor, themeTwoSubColor, themeTwoText, themeTwoBorder, themeTwoNewCallColor, white, black);
+                    break;
+                case "Three":
+                    ThemeChanger(themeThreeMainColor, themeThreeSubColor, themeThreeText, themeThreeBorder, themeThreeNewCallColor, white, black);
+                    break;
+                case "Four":
+                    ThemeChanger(themeFourMainColor, themeFourSubColor, themeFourText, themeFourBorder, themeFourNewCallColor, white, black);
+                    break;
+                case "Default":
+                    ThemeChanger(themeDefaultMainColor, themeDefaultSubColor, themeDefaultText, themeDefaultBorder, themeDefaultNewCallColor, white, black);
+                    break;
+                case "Gavin":
+                    ThemeChanger(themeGavinMainColor, themeGavinSubColor, themeGavinText, themeGavinBorder, themeGavinNewCallColor, white, black);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        //this method takes in the colors and changes the control property based on the parameter passed in
+        private void ThemeChanger(SolidColorBrush mainColor, SolidColorBrush subColor, SolidColorBrush text, SolidColorBrush border, SolidColorBrush newCallColor, SolidColorBrush white,
+            SolidColorBrush black)
+        {
+
+            #region Button List Definition
+            //List of all the buttons
+            List<Button> templateButtons = new List<Button> { auhMuniBtn, cancellationBtn, changeDetailsBtn, delIssueBtn, dispComplaintBtn,
+                exportBtn, irtawiComplaintBtn, labellingBtn, petOrderBtn, smsIssueBtn, sponsorshipBtn, updateJPBtn };
+
+            //List of ShortCut Buttons
+            List<Button> shortcutButton = new List<Button> { shortcutsAbdKbBtn, shortcutsCbdKbBtn, shortcutsCrmBtn, shortcutsPlannedLeavesBtn, shortcutsPowerBiBtn,
+                shortcutsPriceInqBtn, shortcutsRmtMailBtn };
+
+            //List of Menu Buttons
+            List<Button> menuButtons = new List<Button> { extentionsBtn, breakBtn, helpBtn, quitBtn };
+            #endregion
+
+            //set the value for Main Window
+            Background = mainColor;
+
+            //Menu Stackpanel
+            menuStackPanel.Background = subColor;
+            //Menu Buttons
+            foreach (var button in menuButtons)
+            {
+                button.Background = subColor;
+                button.BorderBrush = subColor; // border will always be the background color
+                button.Foreground = text;
+            }
+
+            //New Call Button
+            newCallBtn.Background = newCallColor;
+            newCallBtn.Foreground = white;
+            newCallBtn.BorderBrush = newCallColor;
+
+            //Counter Label
+            counter.Foreground = white;
+
+            //Template Stackpanel
+            templateBtnStackPanel.Background = mainColor;
+            //Template Buttons
+            foreach (var button in templateButtons)
+            {
+                button.Background = mainColor;
+                button.BorderBrush = border;
+                button.Foreground = text;
+            }
+
+            //Main Text Box
+            mainTxtBox.Background = mainColor;
+            mainTxtBox.BorderBrush = white;
+            mainTxtBox.Foreground = text;
+
+            //Shortcuts stackpanel
+            shortcutStackPanel.Background = subColor;
+
+            //Shortcut Buttons
+            foreach (var button in shortcutButton)
+            {
+                button.Background = subColor;
+                button.BorderBrush = subColor; // border will always be the background color
+                button.Foreground = text;
+            }
+        }
     }
+
 }
---
+++ helpWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace CCApp
+{
+    /// <summary>
+    /// Interaction logic for helpWindow.xaml
+    /// </summary>
+    public partial class helpWindow : Window
+    {
+        public helpWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void helpWindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
